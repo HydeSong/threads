e42806ce9a812e5cbc2fa596a54fc5828d2fdbfa
hyde <hyde.song@yahoo.com>
2023-08-07 21:25:02 +0800

    home page

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea";
+
+import * as z from "zod"
+import Image from "next/image";
+import { ChangeEvent, useState } from "react";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
+
+const PostThread = ({ userId }: {
+    userId: string
+}) => {
+    return (
+        <div>PostThread</div>
+    )
+}
+
+export default PostThread
