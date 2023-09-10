48d1f72b000bc410c04a3cf3a28dd2b55be61c43
hyde <hyde.song@yahoo.com>
2023-09-10 21:17:29 +0800

    change

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,32 @@
+"use server"
+
+import {revalidatePath} from "next/cache";
+import {connectToDB} from "../mongoose"
+import User from "../models/user.model"
+import Thread from "../models/thread.model";
+
+interface Params {
+    text : string;
+    author : string;
+    communityId : string | null;
+    path : string;
+}
+
+export async function createThread({text, author, communityId, path} : Params) : Promise < void > {
+    try {
+        connectToDB()
+
+        const createdThread = await Thread.create({text, author, community: null})
+
+        await User.findByIdAndDelete(author, {
+            $push: { 
+                threads: createdThread._id
+            }
+        })
+
+        revalidatePath(path)
+
+    } catch (error : any) {
+        throw new Error(`Failed to create thread: ${error.message}`);
+    }
+}
