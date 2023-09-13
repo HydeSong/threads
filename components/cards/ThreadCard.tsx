454a3e4bd57a86e1e6bb1a5558355df15bea8735
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2023-09-14 01:23:58 +0800

    post thread

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,44 @@
+interface Props {
+    id: string
+    currentUserId: string
+    parentId: string | null
+    content: string
+    author: {
+        name: string
+        image: string
+        id: string
+    }
+    community: {
+        name: string
+        image: string
+        id: string
+    } | null
+    createAt: string
+    comments: {
+        author: {
+            image: string
+        }
+    }[]
+    isComment?: boolean
+}
+
+const ThreadCard = ({
+    id,
+    currentUserId,
+    parentId,
+    content,
+    author,
+    community,
+    createAt,
+    comments
+}: Props) => {
+    return (
+        <article>
+            <h2 className="text-small-regular text-light-2">
+                {content}
+            </h2>
+        </article>
+    )
+}
+
+export default ThreadCard
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,32 +1,60 @@
 "use server"
 
-import {revalidatePath} from "next/cache";
-import {connectToDB} from "../mongoose"
+import { revalidatePath } from "next/cache";
+import { connectToDB } from "../mongoose"
 import User from "../models/user.model"
 import Thread from "../models/thread.model";
 
 interface Params {
-    text : string;
-    author : string;
-    communityId : string | null;
-    path : string;
+    text: string;
+    author: string;
+    communityId: string | null;
+    path: string;
 }
 
-export async function createThread({text, author, communityId, path} : Params) : Promise < void > {
+export async function createThread({ text, author, communityId, path }: Params): Promise<void> {
     try {
         connectToDB()
 
-        const createdThread = await Thread.create({text, author, community: null})
+        const createdThread = await Thread.create({ text, author, community: null })
 
         await User.findByIdAndDelete(author, {
-            $push: { 
+            $push: {
                 threads: createdThread._id
             }
         })
 
         revalidatePath(path)
 
-    } catch (error : any) {
+    } catch (error: any) {
         throw new Error(`Failed to create thread: ${error.message}`);
+    }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    try {
+        connectToDB()
+        const skipAmount = (pageNumber - 1) * pageSize
+        const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+            .sort({ createdAt: 'desc' })
+            .skip(skipAmount)
+            .populate({ path: 'author', model: User })
+            .populate({
+                path: 'children',
+                populate: {
+                    path: 'author',
+                    model: User,
+                    select: '_id name parentId image'
+                }
+            })
+
+        const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+
+        const posts = await postsQuery.exec()
+        const isNext = totalPostsCount > skipAmount + posts.length
+
+        return {posts, isNext}
+    } catch (error: any) {
+        throw new Error(`Failed to fetch threads: ${error.message}`);
     }
 }
