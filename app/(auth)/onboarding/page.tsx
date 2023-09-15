1c55c082cc4a406360b6de08dde1a00109d8dc32
Hyde Song <hyde@Hydes-MacBook-Pro.local>
2023-09-16 01:49:34 +0800

    fix

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,16 +1,22 @@
 import AccountProfile from "@/components/forms/AccountProfile"
+import { fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
-const Page = async() => {
+const Page = async () => {
     const user = await currentUser()
-    const userInfo = {}
+    if (!user) return null
+
+    const userInfo = await fetchUser(user.id)
+    if (userInfo?.onboarded) redirect('/')
+
     const userData = {
         id: user?.id,
         objectId: userInfo?._id,
-        username: userInfo?.username || user?.username,
-        name: userInfo?.name || user?.firstName || "",
-        bio: userInfo?.bio || "",
-        image: userInfo?.image || user?.imageUrl,
+        username: userInfo ? userInfo?.username : user?.username,
+        name: userInfo ? userInfo?.name : user?.firstName || "",
+        bio: userInfo ? userInfo?.bio : "",
+        image: userInfo ? userInfo?.image : user?.imageUrl,
     };
 
     return (
@@ -18,7 +24,7 @@ const Page = async() => {
             <h1 className="head-text">Onboarding</h1>
             <p className="mt-3 text-base-regular text-light-2">Complete your profile now, to use Threads.</p>
             <section className="mt-9 bg-dark-2 p-10">
-                <AccountProfile user={userData} btnTitle="Continue"/>
+                <AccountProfile user={userData} btnTitle="Continue" />
             </section>
         </main>
     )
